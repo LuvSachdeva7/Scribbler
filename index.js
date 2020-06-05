a6713fe67866e2100adc83c981803ca93865411c
LUV <luv@puppet-master.local>
2020-06-05 17:04:05 -0400

    Createpost modal is created

--- index.js
+++ index.js
@@ -18,3 +18,12 @@ function open_signin_modal(){
     var signin_modal=document.getElementById("signin_page");
     signin_modal.style.display="block";
 }
+function close_createposts_modal() {
+var createposts=document.getElementById("create_posts_page")
+createposts.style.display="none";
+}
+function open_createposts_modal() {
+    var createposts=document.getElementById("create_posts_page")
+    createposts.style.display="block";
+
+}
