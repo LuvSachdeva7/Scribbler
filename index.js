87be625af2188ca3e1d1706d124338cc26cd36ef
LUV <luv@puppet-master.local>
2020-06-05 08:24:53 -0400

    project1 is added

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("My name is lucv")
+function close_modal(){
+    var signup_modal=document.getElementById("signup_page");
+    signup_modal.style.display="none";
+}
+function open_modal(){
+    var signup_modal=document.getElementById("signup_page");
+    signup_modal.style.display="block";
+}
---
+++ postlist.js
@@ -0,0 +1,12 @@
+
+function delete_post(){
+    var post=document.getElementById("post4");
+    console.log("Delete is clicked")
+    post.style.display="none";
+}
+
+function luv(){
+    var modal1=document.getElementById("modal")
+    console.log("Delete is clicked");
+    modal1.style.display="block";
+}
