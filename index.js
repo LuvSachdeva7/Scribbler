2f1f63a634c9fe03298cc36333d0800e650440aa
LUV <luv@puppet-master.local>
2020-06-05 10:54:43 -0400

    project1 is added

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-console.log("My name is lucv")
-function close_modal(){
+
+function close_signup_modal(){
     var signup_modal=document.getElementById("signup_page");
     signup_modal.style.display="none";
 }
-function open_modal(){
+function open_signup_modal(){
+    var signin_modal=document.getElementById("signin_page");
+    signin_modal.style.display="none";
     var signup_modal=document.getElementById("signup_page");
     signup_modal.style.display="block";
+}
+function close_signin_modal(){
+    var signin_modal=document.getElementById("signin_page");
+    signin_modal.style.display="none";
+}
+function open_signin_modal(){
+
+    var signin_modal=document.getElementById("signin_page");
+    signin_modal.style.display="block";
 }
