a2059aa577be11b79bdba951caa790a16c8b37d8
LuvSachdeva7 <sachdeva.luv1993@gmail.com>
2020-06-07 14:19:47 -0400

    Automatic Adjustment After Deletion of box

--- postlist.js
+++ postlist.js
@@ -1,16 +1,21 @@
 
-function delete_post(clicked){
-    console.log(clicked)
-    alert(clicked);
+function delete_post(){
+    
     // var post=document.getElementById("this.id");
     // console.log("Delete is clicked")
     // post.style.display="none";
 }
 
-function luv(clicked){
-    console.log(clicked);
-    alert(clicked);
+function luv(clicked_icon){
+    console.log(clicked_icon)
+    var clicked=document.getElementById(clicked_icon);
+    var r1=clicked.closest(".post-layout");
+    console.log(r1);
+    r1.style.display="none";
+    // clicked.style.display="none";
     // var modal1=document.getElementById("modal")
     // console.log("Delete is clicked");
     // modal1.style.display="block";
+    
+
 }
