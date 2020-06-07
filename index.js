6b1970b5de4031e4feefb18506084607c7d24374
LuvSachdeva7 <sachdeva.luv1993@gmail.com>
2020-06-07 12:13:12 -0400

    Project2 layout is designed

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 
-function close_signup_modal(){
-    var signup_modal=document.getElementById("signup_page");
-    signup_modal.style.display="none";
+function close_signup_modal() {
+    var signup_modal = document.getElementById("signup_page");
+    signup_modal.style.display = "none";
 }
-function open_signup_modal(){
-    var signin_modal=document.getElementById("signin_page");
-    signin_modal.style.display="none";
-    var signup_modal=document.getElementById("signup_page");
-    signup_modal.style.display="block";
+function open_signup_modal() {
+    var signin_modal = document.getElementById("signin_page");
+    signin_modal.style.display = "none";
+    var signup_modal = document.getElementById("signup_page");
+    signup_modal.style.display = "block";
 }
-function close_signin_modal(){
-    var signin_modal=document.getElementById("signin_page");
-    signin_modal.style.display="none";
+function close_signin_modal() {
+    var signin_modal = document.getElementById("signin_page");
+    signin_modal.style.display = "none";
 }
-function open_signin_modal(){
+function open_signin_modal() {
 
-    var signin_modal=document.getElementById("signin_page");
-    signin_modal.style.display="block";
+    var signin_modal = document.getElementById("signin_page");
+    signin_modal.style.display = "block";
 }
 function close_createposts_modal() {
-var createposts=document.getElementById("create_posts_page")
-createposts.style.display="none";
+    var createposts = document.getElementById("create_posts_page")
+    createposts.style.display = "none";
 }
 function open_createposts_modal() {
-    var createposts=document.getElementById("create_posts_page")
-    createposts.style.display="block";
+    var createposts = document.getElementById("create_posts_page")
+    createposts.style.display = "block";
 
 }
--- postlist.js
+++ postlist.js
@@ -1,12 +1,16 @@
 
-function delete_post(){
-    var post=document.getElementById("post4");
-    console.log("Delete is clicked")
-    post.style.display="none";
+function delete_post(clicked){
+    console.log(clicked)
+    alert(clicked);
+    // var post=document.getElementById("this.id");
+    // console.log("Delete is clicked")
+    // post.style.display="none";
 }
 
-function luv(){
-    var modal1=document.getElementById("modal")
-    console.log("Delete is clicked");
-    modal1.style.display="block";
+function luv(clicked){
+    console.log(clicked);
+    alert(clicked);
+    // var modal1=document.getElementById("modal")
+    // console.log("Delete is clicked");
+    // modal1.style.display="block";
 }
